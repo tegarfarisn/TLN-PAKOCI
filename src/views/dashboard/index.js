92db3690a0e4d0ffa75745cb1f2c3826459b8be9
ikramattamimi <ikram.muhammad.tif20@polban.ac.id>
2022-06-04 17:09:16 +0700

    dashboard jumlah penguji pendaftar per periode

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Grid} from '@material-ui/core';
-import {useState} from 'react';
+import { Grid } from '@material-ui/core';
+// import {useState} from 'react';
 import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -11,97 +11,112 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
-import { Card, CardHeader, Typography,TextField, Button,MenuItem, CardContent,Select, InputLabel, FormControl} from '@mui/material';
+import { Card, CardHeader, Typography, TextField, Button, MenuItem, CardContent, Select, InputLabel, FormControl } from '@mui/material';
+import * as URL from '../../api/index'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 
 const Dashboard = () => {
   const [bulan, setBulan] = useState('');
-
-  const url = "http://10.50.164.137:1337/api/pendaftars?populate[0]=pegawai.jabatan&populate[1]=pegawai.grade&populate[2]=pegawai.jenjang"
   const [posts, setPosts] = useState([])
+  const [jumlahPenguji, setJumlahPenguji] = useState('')
+  const [jumlahPendaftar, setJumlahPendaftar] = useState('')
+  const [periode, setPeriode] = useState('Juni 2022')
+  const [listPeriode, setListPeriode] = useState([])
 
-  const handleChange= (event) => {
+  const handleChange = (event) => {
     setBulan(event.target.value);
-  } 
+  }
 
-    return (
-     
-      <Grid container>
-       <Typography variant="h2" xs={12} md ={2.2} paddingBottom={2}>Welcome to Fit & Proper </Typography>
+  useEffect(() => {
+    getJumlahPengujiBulanIni();
+    getJumlahPendaftarBulanIni();
+    getListPeriode();
+  }, [periode])
+
+  const getJumlahPengujiBulanIni = () => {
+    console.log("get jumlah penguji bulan ini");
+    axios.get(URL.baseURL + `/pengujis?filters[periode][periode][$eq]=${periode}`)
+      .then((res) => {
+        console.log("ini penguji", res.data.data);
+        console.log("jumlah penguji: ", res.data.data.length)
+        setJumlahPenguji(res.data.data.length);
+      })
+  }
+
+  const getJumlahPendaftarBulanIni = () => {
+    console.log("get jumlah pendaftar bulan ini");
+    axios.get(URL.baseURL + `/pendaftars?filters[periode][periode][$eq]=${periode}`)
+      .then((res) => {
+        console.log("ini pendaftar", res.data.data);
+        console.log("jumlah pendaftar: ", res.data.data.length)
+        setJumlahPendaftar(res.data.data.length);
+      })
+  }
+
+  const getListPeriode = () => {
+    console.log("get list periode");
+    axios.get(URL.baseURL + `/periodes`)
+      .then((res) => {
+        console.log("ini periode", res.data.data);
+        setListPeriode(res.data.data);
+      })
+  }
+
+
+
+  return (
+
+    <Grid container>
+      <Typography variant="h2" xs={12} md={2.2} paddingBottom={2}>Welcome Admin! </Typography>
       <Grid item xs={12}>
-      <Card>
-          <CardHeader title={<Typography variant="h4"> </Typography>} />
-          <Grid container spacing={1} paddingBottom={3}>
-            <Grid item xs={2} md ={2.2}>
-                <Typography variant = "h5" paddingLeft={3}  display='center' >
-                  Pilih Bulan
-                </Typography> 
-            </Grid>
-            <Grid item xs={1} md ={2.2}>           
-              <FormControl fullWidth>
-              <InputLabel id = "Pilih Bulan Tahun" >
-                Pilih Bulan Tahun
-              </InputLabel>
-                  <Select
-                  labelId="bulan"
-                  id="bulan"
-                  value={bulan}
-                  label="bulan"
-                  onChange={handleChange}>
-                      <MenuItem value={10}>Januari 2022</MenuItem>
-                      <MenuItem value={20}>Februari 2022</MenuItem>
-                      <MenuItem value={20}>Maret 2022</MenuItem>
-                      <MenuItem value={20}>April 2022</MenuItem>
-                      <MenuItem value={20}>Mei 2022</MenuItem>
-                      <MenuItem value={20}>Juni 2022</MenuItem>
-                      <MenuItem value={20}>Juli 2022</MenuItem>
-                      </Select>
-              </FormControl>
-              </Grid> 
-              <Grid item xs={2} md ={2.2}>
-                <Button variant="contained" md={1} type="submit" >
-                    CEK
-                </Button> 
-              </Grid>
-          </Grid>
+        <Card>
+          <CardHeader title={<Typography variant="h1"> Pilih Periode Fit n Proper</Typography>} />
           <CardContent>
-          <Stack direction="row" alignItems="center" gap={4}   >
-            <CalendarMonthOutlinedIcon md ={2.2}/>
-            <Typography variant="h4">Jadwal Fit & Proper</Typography>
-          </Stack>
+            <Grid container spacing={1} paddingBottom={3}>
+              <Grid item xs={2} md={2.2}>
+                <Typography variant="h5" paddingLeft={3} display='center' >
+                  Pilih Bulan Tahun
+                </Typography>
+              </Grid>
+              <Grid item xs={1} md={2.2}>
+                <FormControl fullWidth>
+                  <InputLabel id="Pilih Bulan Tahun" >
+                    Pilih Bulan Tahun
+                  </InputLabel>
+                  <Select
+                    labelId="bulan"
+                    id="bulan"
+                    value={periode}
+                    label="bulan"
+                    onChange={(e) => setPeriode(e.target.value)}>
+                    {listPeriode.map(list =>
+                      <MenuItem value={list.attributes.periode}>{list.attributes.periode}</MenuItem>
+                    )}
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
           </CardContent>
-         
-          <TableContainer component={Paper}>  
-          <Table sx={{ minWidth: 10 }} aria-label="simple table">
-             <TableHead>
-              <TableRow>
-                <TableCell >No</TableCell>
-                <TableCell align="right">Nama</TableCell>
-                <TableCell align="right">NIP</TableCell>
-                <TableCell align="right">Jabatan</TableCell>
-                <TableCell align="right">Proyeksi</TableCell>
-                <TableCell align="right">Tanggal</TableCell>
-                <TableCell align="right">Penguji</TableCell>
-              </TableRow>
-            </TableHead>
-              {posts.map(post => 
-                <TableBody>
-                  <TableRow>
-                    <TableCell>{post.attributes.pegawai.data.id}</TableCell>
-                    <TableCell align="right">{post.attributes.pegawai.data.attributes.nama}</TableCell>
-                    <TableCell align="right">{post.attributes.pegawai.data.attributes.nip}</TableCell>
-                    <TableCell align="right">{post.attributes.pegawai.data.attributes.jenjang.data.attributes.jenjang_jabatan_struktural}</TableCell>
-                    {/* <TableCell align="right">{post.attributes.pegawai.data.attributes.grade.data.attributes.nama_grade}</TableCell>
-                    <TableCell align="right">{post.attributes.pegawai.data.attributes.jabatan.data.attributes.nama_jabatan}</TableCell> */}
-                  </TableRow>
-                </TableBody>
-                )}
-          </Table>
-        </TableContainer> 
-            </Card>
-          </Grid>
-        </Grid>
-    );
+        </Card>
+        <br></br>
+        <Card marginTop={3}>
+          <CardHeader mt={3} title={<Typography variant="h1"> Apa ya </Typography>} />
+          <CardContent>
+            <Stack direction="row" alignItems="center" gap={4}   >
+              <Typography variant="h4">Jumlah Penguji: {jumlahPenguji}</Typography>
+            </Stack>
+          </CardContent>
+          <CardContent>
+            <Stack direction="row" alignItems="center" gap={4}   >
+              <Typography variant="h4">Jumlah Pendaftar: {jumlahPendaftar}</Typography>
+            </Stack>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Dashboard;
